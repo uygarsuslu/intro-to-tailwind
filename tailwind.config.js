714051c448217a38390750d90977f0764290fe0f
Uygar Süslü <uygarsuslu1@gmail.com>
2022-09-20 13:10:57 +0300

    header layout added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        prototurk: "#32445a",
+        twitter: "#1da1f2"
+      },
+      spacing: {
+        "15": "3.75rem"
+      },
+    },
   },
   plugins: [],
 }
